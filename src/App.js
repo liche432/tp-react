6222e3a0c606e1a0e4c636465d3cc27ef6ec41c3
Greg43Faure <gregory.faure@almarena-it.fr>
2022-03-29 11:48:33 +0200

    TP route
    Changement et installation des packages pour le tp sur les routes

--- src/App.js
+++ src/App.js
@@ -5,66 +5,73 @@ import Test from "./components/Test";
 import Movie from "./components/Movie"
 import toto from './components/Movie.module.css'
 
-const App = () => {
-    const [myVar, setMyVar] = useState('Hello word') // permet de mettre a jour la vue (c'est un hook)
-    const [movies, setMovies] = useState([]);
+// Commenté pour l'activité des routes
+// const App = () => {
+//     const [myVar, setMyVar] = useState('Hello word') // permet de mettre a jour la vue (c'est un hook)
+//     const [movies, setMovies] = useState([]);
+//
+//
+//     // let myVar = "hello world"
+//     const onClickHandler = () => {
+//         // console.log('clicked');
+//         // myVar = "Goodbye";
+//         // console.log = "Goodbye";
+//       setMyVar('Hello Word updated');
+//
+//       console.log(myVar); // on sse rend compte qu'il a encore "Hello Word"
+//
+//       setMyVar((prevState) => {
+//          return prevState + ' updated'; // il est conseiller d'utiliser cette syntaxe pour travailler avec la dernière version
+//       });
+//     }
+//
+//     const childToParentUpdater = (data) => {
+//         console.log('childToParentUpdater', data)
+//     }
+//
+//     function fetchMoviesHandler(){
+//         fetch('https://swapi.dev/api/films/') // on récupère des données de ce site
+//         .then(response => {
+//             console.log(response);
+//             return response.json();
+//         }).then(data => {
+//             console.log(data);
+//             setMovies(data.results);
+//         })
+//     }
+//
+//     useEffect(() => {
+//         //document.title = myVar //on change le titre pour pouvoir toujours l'accès à onClickHandler a la fonction. Sinon, comme il n'y a aucun changement, on y a accès 1 fois
+//         //console.log('useEffect');
+//         fetchMoviesHandler();
+//     }, [myVar]); // on l'utilise pour des appels http
+//
+//
+//
+//
+//     return (
+//         <div className="App">
+//             <button onClick = {onClickHandler}>Click Me</button>
+//             <Test myProps={myVar} updater={childToParentUpdater} /> {/*on appel notre components*/}
+//             {myVar}
+//             <div className={toto.generalMovie}>
+//                 {movies.map( (movie, index) => { // quand on ne sait pas quoi mettre dans key on peut utiliser un autre paramètre index
+//                     console.log(movie);
+//                     return <Movie key={index} myTitle={movie.title} myDescription={movie.opening_crawl} />;
+//                 }) }
+//             </div>
+//
+//         </div>
+//     );
+// }
 
-
-    // let myVar = "hello world"
-    const onClickHandler = () => {
-        // console.log('clicked');
-        // myVar = "Goodbye";
-        // console.log = "Goodbye";
-      setMyVar('Hello Word updated');
-
-      console.log(myVar); // on sse rend compte qu'il a encore "Hello Word"
-
-      setMyVar((prevState) => {
-         return prevState + ' updated'; // il est conseiller d'utiliser cette syntaxe pour travailler avec la dernière version
-      });
-    }
-
-    const childToParentUpdater = (data) => {
-        console.log('childToParentUpdater', data)
-    }
-
-    function fetchMoviesHandler(){
-        fetch('https://swapi.dev/api/films/') // on récupère des données de ce site
-        .then(response => {
-            console.log(response);
-            return response.json();
-        }).then(data => {
-            console.log(data);
-            setMovies(data.results);
-        })
-    }
-
-    useEffect(() => {
-        //document.title = myVar //on change le titre pour pouvoir toujours l'accès à onClickHandler a la fonction. Sinon, comme il n'y a aucun changement, on y a accès 1 fois
-        //console.log('useEffect');
-        fetchMoviesHandler();
-    }, [myVar]); // on l'utilise pour des appels http
-
-    
-
-
+export default function App(){
     return (
-        <div className="App">
-            <button onClick = {onClickHandler}>Click Me</button>
-            <Test myProps={myVar} updater={childToParentUpdater} /> {/*on appel notre components*/}
-            {myVar}
-            <div className={toto.generalMovie}>
-                {movies.map( (movie, index) => { // quand on ne sait pas quoi mettre dans key on peut utiliser un autre paramètre index
-                    console.log(movie);
-                    return <Movie key={index} myTitle={movie.title} myDescription={movie.opening_crawl} />;
-                }) }
-            </div>
-            
+        <div>
+            <h1>Bookkeeper !</h1>
         </div>
     );
-}
-
-export default App;
+};
 
 
 /* différence entre un import de module css et utilisation de class classique
